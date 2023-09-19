bf51eed440cf675333260b6ae628ff973e7dca17
Rostislav <padafarostislav@gmail.com>
2023-09-19 14:55:05 +0300

    LabWork

---
+++ LabWork1/LabWork1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    const char x{ 6 };
+    char y{ 8 };
+    const float z{ 9.5 };
+
+    double power;
+    {
+        power = pow(y, 2);
+        cout << "Squared number y = " << power << endl;
+    }
+
+    double remainder;
+    {
+        remainder = fmod(z, x);
+        cout << "\nRemainder of z%x = " << 9.5 << "/" << 6 << " = " << remainder << endl;
+    }
+    
+    const double result{ (remainder - 1.5) * (x + 34 * pow(y,2) - 4.3 * z / x) / (12 * z) };
+
+    cout << "\nResult = " << result << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
---
+++ LabWorkVer.2/LabWorkVer.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char x{ 6 }, y{ 8 };
+	const char y2{ 64 };
+	float z{ 9.5 };
+
+	double remainder = fmod(z, x);
+	cout << "Remainder = " << remainder << endl;
+
+	const double result{ (remainder - 1.5) * (x + 34 * y - 4.3 * z / x) / (12 * z) };
+	const double result2{ (remainder - 1.5) * (x + 34 * y2 - 4.3 * z / x) / (12 * z) };
+
+	cout << "\nResult = " << result << endl;
+	cout << "\nResult2 = " << result2 << endl;
+	
+	return 0;
+}
